0ca8d68718ddf2f3c82de853915e1736529c4833
stan martens <Stan Martens>
2018-03-22 17:45:28 +0100

    Add tests for starting a new game

--- src/test/java/SeaBattleGameTest.java
+++ src/test/java/SeaBattleGameTest.java
@@ -1,4 +1,43 @@
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import seabattlegame.ISeaBattleGame;
+import seabattlegame.SeaBattleGame;
+import seabattlegui.ISeaBattleGUI;
+import seabattlegui.SeaBattleApplication;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SeaBattleGameTest {
+
+    private ISeaBattleGame seaBattleGame;
+    private ISeaBattleGUI seaBattleGUI;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Before
+    public void testInitialize() {
+        seaBattleGame = new SeaBattleGame();
+        seaBattleGUI = new SeaBattleApplication();
+    }
+
+    @Test
+    public void testStartNewGame() {
+        int playerId = seaBattleGame.registerPlayer("John doe", seaBattleGUI, true);
+        assertTrue(seaBattleGame.startNewGame(playerId));
+    }
+
+    @Test
+    public void testStartNewGameInvalidPlayer() {
+        exception.expect(IllegalArgumentException.class);
+        seaBattleGame.startNewGame(2);
+    }
+
+    @Test
+    public void testStartNewGameStartTwoPlayerGameWithOnlyOnePlayer() {
+
+    }
 }
