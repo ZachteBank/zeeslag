a663ac67bb53d284c47c7b2ef21fbdc16d8932d7
Martens,Stan S.W.T <stan.martens@student.fontys.nl>
2018-03-23 10:24:02 +0100

    add tests for shipfactory

--- src/test/java/ShipFactoryTest.java
+++ src/test/java/ShipFactoryTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 import seabattlegame.game.shipfactory.ShipFactory;
-import seabattlegame.game.ships.AircraftCarrier;
+import seabattlegame.game.ships.*;
 import seabattlegui.ShipType;
 
 import static org.junit.Assert.*;
@@ -13,4 +13,32 @@ public class ShipFactoryTest {
         assertEquals(carrier.getType(), ShipFactory.createShip(ShipType.AIRCRAFTCARRIER).getType());
     }
 
+    @Test
+    public void testCreateShipCruiser() {
+        Cruiser cruiser = new Cruiser();
+        assertEquals(cruiser.getType(), ShipFactory.createShip(ShipType.CRUISER).getType());
+    }
+
+    @Test
+    public void testCreateShipSubmarine() {
+        Submarine submarine = new Submarine();
+        assertEquals(submarine.getType(), ShipFactory.createShip(ShipType.SUBMARINE).getType());
+    }
+
+    @Test
+    public void testCreateShipMinesweeper() {
+        Minesweeper minesweeper = new Minesweeper();
+        assertEquals(minesweeper.getType(), ShipFactory.createShip(ShipType.MINESWEEPER).getType());
+    }
+
+    @Test
+    public void testCreateShipBattleship() {
+        Battleship battleship = new Battleship();
+        assertEquals(battleship.getType(), ShipFactory.createShip(ShipType.BATTLESHIP).getType());
+    }
+
+    @Test
+    public void testCreateShipShipIsNull() {
+        ShipFactory.createShip(null);
+    }
 }
