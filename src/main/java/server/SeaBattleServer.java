a95a00bd74b2cf63178aef1f7cf82db696f2749c
bramk <bramkempen@gmail.com>
2018-04-04 09:41:21 +0200

    add server start

---
+++ src/main/java/server/SeaBattleServer.java
@@ -0,0 +1,33 @@
+package server;
+
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+import javax.websocket.server.ServerContainer;
+
+
+public class SeaBattleServer {
+    public static void main(String[] args) {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8085);
+        server.addConnector(connector);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+
+        try {
+            ServerContainer container = WebSocketServerContainerInitializer.configureContext(context);
+            container.addEndpoint(EventServerSocket.class);
+            server.start();
+            server.join();
+        }catch (Throwable t){
+            t.printStackTrace(System.err);
+        }
+    }
+
+}
