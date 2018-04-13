a55e09a5ebe9b195c394eb81ca81291e3667e614
bramk <bramkempen@gmail.com>
2018-04-13 12:45:42 +0200

    add placeship

---
+++ src/main/java/server/json/actions/placeShip.java
@@ -0,0 +1,31 @@
+package server.json.actions;
+
+public class placeShip implements IAction {
+    private int x;
+    private int y;
+    private boolean horizontal;
+    private String shipType;
+
+    public placeShip(int x, int y, boolean horizontal, String shipType) {
+        this.x = x;
+        this.y = y;
+        this.horizontal = horizontal;
+        this.shipType = shipType;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isHorizontal() {
+        return horizontal;
+    }
+
+    public String getShipType() {
+        return shipType;
+    }
+}
