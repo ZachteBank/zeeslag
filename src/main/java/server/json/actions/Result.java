0fbaaea8a2a4f98abc8fac407eae4c2727131654
Martens,Stan S.W.T <stan.martens@student.fontys.nl>
2018-04-17 09:18:00 +0200

    Add message for result boolean

---
+++ src/main/java/server/json/actions/Result.java
@@ -0,0 +1,14 @@
+package server.json.actions;
+
+public class Result {
+
+    private boolean result;
+
+    public Result(boolean result) {
+        this.result = result;
+    }
+
+    public boolean getResult() {
+        return result;
+    }
+}
